93b2acb721a74925eb9579f1408572ff22d68199
k7rish <100378183+k7rish@users.noreply.github.com>
2022-07-11 12:11:05 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sshpass -p '123456' ssh -o StrictHostKeyChecking=no ubuntu@3.89.145.89 "(
+sshpass -p '123456' ssh -o StrictHostKeyChecking=no ubuntu@3.110.148.170
 sudo docker run -dit -p 4000:3000 605580347753.dkr.ecr.ap-south-1.amazonaws.com/java:latest
 sudo docker ps
 )"
