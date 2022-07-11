cc366ee0b65d7946dc6fa939b9821df18f16ddfa
k7rish <100378183+k7rish@users.noreply.github.com>
2022-07-11 12:50:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sshpass -p '123456' ssh -o StrictHostKeyChecking=no ubuntu@3.110.148.170 "(
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 605580347753.dkr.ecr.ap-south-1.amazonaws.com
 sudo docker run -dit -p 4000:3000 605580347753.dkr.ecr.ap-south-1.amazonaws.com/java:latest
 sudo docker ps
 )"
